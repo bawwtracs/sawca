50e9961de85b31a01fc927b60e84c8146726846e
bawwtracs <bawwtracs@outlook.com>
2020-09-24 21:28:39 +0800

    update

--- support/src/main/java/sawca/support/vocabulary/entity/Word.java
+++ support/src/main/java/sawca/support/vocabulary/entity/Word.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
@@ -29,7 +26,7 @@ public class Word implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
